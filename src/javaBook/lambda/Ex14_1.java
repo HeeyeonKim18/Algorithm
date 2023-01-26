4242b7667dd74f48d46a9af21d652bff4687c4f3
HeeyeonKim18 <go1927hm@gmail.com>
2023-01-26 11:24:24 +0900

    write some code on Ex14_1 file

--- src/javaBook/lambda/Ex14_1.java
+++ src/javaBook/lambda/Ex14_1.java
@@ -1,4 +1,38 @@
 package javaBook.lambda;
 
+@FunctionalInterface
+interface MyFunction1 {
+    void run();
+}
+
 public class Ex14_1 {
+
+    static void execute(MyFunction1 f) {
+        f.run();
+    }
+
+    static MyFunction1 getMyFunction() {
+        return () -> System.out.println("f3.run()");
+    }
+
+    public static void main(String[] args) {
+
+        MyFunction1 f1 = () -> System.out.println("f1.run()");
+
+        MyFunction1 f2 = new MyFunction1() {
+            @Override
+            public void run() {
+                System.out.println("f2.run()");
+            }
+        };
+
+        MyFunction1 f3 = getMyFunction();
+
+        f1.run();
+        f2.run();
+        f3.run();
+
+        execute(f1);
+        execute(() -> System.out.println("run()"));
+    }
 }
