9862fcda70e6fd6888b42eebe95e9e48fb4831db
HeeyeonKim18 <go1927hm@gmail.com>
2023-01-31 11:54:53 +0900

    perform Main 10870 recursion

--- src/main/recursion/Main10870.java
+++ src/main/recursion/Main10870.java
@@ -1,7 +1,21 @@
 package main.recursion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main10870 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(f(N));
+    }
+
+    private static int f(int n) {
+        if(n < 2)
+            return n;
+        else
+            return f(n-1) + f(n-2);
     }
 }
