c0f43852648d41f19b385f225c678f2543106433
HeeyeonKim18 <go1927hm@gmail.com>
2023-01-31 11:26:54 +0900

    perform main 10872

--- src/main/recursion/Main10872.java
+++ src/main/recursion/Main10872.java
@@ -1,4 +1,21 @@
 package main.recursion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(recursion(N));
+    }
+
+    private static int recursion(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * recursion(n - 1);
+    }
 }
