dab369a4d359821d284f9e3edaa2a1b31271a705
Heeyeon <go1927hm@gmail.com>
2023-02-11 10:05:53 +0900

    do left stuff

---
+++ src/main/bruteforce/Main1436.java
@@ -0,0 +1,23 @@
+package main.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 1;
+        int num = 666;
+        while (count != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
