ab09e7f109ce150e521b6a70bc141f108469b67e
HeeyeonKim18 <go1927hm@gmail.com>
2023-03-17 12:07:08 +0900

    Main1149

--- src/main/dynamicProgramming/Main1149_bottomUp.java
+++ src/main/dynamicProgramming/Main1149_bottomUp.java
@@ -10,11 +10,6 @@ import java.util.StringTokenizer;
  * 반복문을 활용하여 문제를 푸는 방식
  */
 public class Main1149_bottomUp {
-
-    final static int RED = 0;
-    final static int GREEN = 1;
-    final static int BLUE = 2;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -25,20 +20,20 @@ public class Main1149_bottomUp {
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");   // 배열 값 저장하기
 
-            cost[i][RED] = Integer.parseInt(st.nextToken());
-            cost[i][GREEN] = Integer.parseInt(st.nextToken());
-            cost[i][BLUE] = Integer.parseInt(st.nextToken());
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i < N; i++) {  // 각 위치에서 이전 값의 비교해 최소합 만들기
-            cost[i][RED] += Math.min(cost[i - 1][GREEN], cost[i - 1][BLUE]);
-            cost[i][GREEN] += Math.min(cost[i - 1][RED], cost[i - 1][BLUE]);
-            cost[i][BLUE] += Math.min(cost[i - 1][RED], cost[i - 1][GREEN]);
+            cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+            cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+            cost[i][2] += Math.min(cost[i - 1][0], cost[i - 1][1]);
 
         }
 
         // 배열에서 최소합을 찾아 출력하기
-        System.out.println(Math.min(cost[N - 1][RED], Math.min(cost[N - 1][GREEN], cost[N - 1][BLUE])));
+        System.out.println(Math.min(cost[N - 1][0], Math.min(cost[N - 1][1], cost[N - 1][2])));
 
     }
 }
