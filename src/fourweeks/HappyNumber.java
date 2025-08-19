434dfd90b6d95067c394c2486a1248e49fd612b2
HeeyeonKim18 <go1927hm@gmail.com>
2025-08-19 17:57:39 +0900

    14days practice

--- src/fourweeks/HappyNumber.java
+++ src/fourweeks/HappyNumber.java
@@ -1,5 +1,8 @@
 package fourweeks;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
     public static void main(String[] args) {
         int n = 19;
@@ -7,6 +10,24 @@ public class HappyNumber {
     }
 
     private static boolean isHappy(int n) {
-        return true;
+        Set<Integer> set = new HashSet<>();
+
+        while (!set.contains(n)) {
+            set.add(n);
+            n = getNextNumber(n);
+            if (n == 1)
+                return true;
+        }
+        return false;
+    }
+
+    private static int getNextNumber(int n) {
+        int output = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            output += digit * digit;
+            n = n / 10;
+        }
+        return output;
     }
 }
