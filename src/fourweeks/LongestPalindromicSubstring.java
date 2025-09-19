06a92822fd70c02316880d9ea0ec42fd23a84c58
HeeyeonKim18 <go1927hm@gmail.com>
2025-09-19 17:22:41 +0900

    24days practice

---
+++ src/fourweeks/LongestPalindromicSubstring.java
@@ -0,0 +1,38 @@
+package fourweeks;
+
+/**
+ * 가장 긴 palindrom substring 찾기
+ * 입력 값: 1 <= s.length <= 1000, only digits and English letters(can be Upper or
+ * Lower cases)
+ */
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+        System.out.println(longestPalindrome("cbbd"));
+    }
+
+    private static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return "";
+
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int odd = expand(s, i, i);
+            int even = expand(s, i, i + 1);
+            int len = Math.max(odd, even);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expand(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+}
