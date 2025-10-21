a975b72e61d9ad9d28f0b2303d36c56ca05d3a6d
HeeyeonKim18 <go1927hm@gmail.com>
2025-10-21 13:26:51 +0900

    addTwoNumbers

---
+++ src/fourweeks/additional/AddTwoNumbers.java
@@ -0,0 +1,51 @@
+package fourweeks.additional;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        addTwoNumbers(l1, l2);
+    }
+
+    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            cur.next = new ListNode(sum % 10); // 새 노드 생성해서 연결
+            cur = cur.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
